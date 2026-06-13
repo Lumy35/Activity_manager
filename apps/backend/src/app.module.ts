83fff31a7602acb82e6da6c11e3fc6163fecb37b
LuanSilva <lc.sillva1404@gmail.com>
2026-06-13 13:25:00 -0300

    feat: gera modulos iniciais para as entidades

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { SprintsModule } from './modules/sprints/sprints.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, SprintsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
